9997dfd29ca56042c8679ac7e5c055899bf9b3fb
arimuraterutoshiMac <arimuraterutoshi@192.168.11.6>
2017-05-03 22:24:19 +0900

    子のコンポーネントにstoreをdi

--- src/components/child-component.js
+++ src/components/child-component.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
+import * as SampleAction from '../actions/sample-action';
 
+function mapStateToProps() {
+  return {}
+}
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators( Object.assign({}, SampleAction), dispatch);
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class ChildComponent extends Component {
   static propTypes = {
+    change_text: PropTypes.func.isRequired,
     copyText: PropTypes.string.isRequired
   }
   static defaultProps = {
@@ -13,11 +25,16 @@ export default class ChildComponent extends Component {
     super(props);
   }
 
+  clearText() {
+    this.props.change_text("")
+  }
+
   render() {
     const { copyText } = this.props;
     return (
       <div>
-        <label>{ copyText }</label>
+        <label>{ copyText }</label><br />
+        <button onClick = { this.clearText.bind(this) }>クリア</button>
       </div>
     );
   }
