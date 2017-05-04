d3cbd08b8e98f54f8db401f582b1408ecdaa2a5d
arimuraterutoshiMac <arimuraterutoshi@192.168.11.6>
2017-05-04 12:44:58 +0900

    リアクトルーター(v4.1.1)を試してみる

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import FirstComponent from './components/first-component'
 import StoreConfig from './store/store-config'
 
 import './assets/bootstrap/css/bootstrap.min.css'
 import './assets/css/main.css'
 
+import {  BrowserRouter, Route, Switch } from 'react-router-dom';
+import FirstConponent from './components/first-component';
+import ListComponent from './components/list-component';
+
+
 const store = StoreConfig()
+
 render(
   <Provider store={store}>
-    <FirstComponent />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={FirstConponent} />
+        <Route exact path="/first" component={FirstConponent} />
+        <Route exact path="/list" component={ListComponent} />
+      </Switch>
+    </BrowserRouter>
   </Provider >
   , document.getElementById('app')
 );
