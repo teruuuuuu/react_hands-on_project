6c72cef1d899f071c828bf01a788011fd1a2ad2a
arimuraterutoshiMac <arimuraterutoshi@192.168.11.6>
2017-05-04 13:19:55 +0900

    ログ出力のミドルウェア追加

---
+++ src/midleware/logger.js
@@ -0,0 +1,8 @@
+const logger = function actionDebugMiddleware() {
+  return next => action => {
+    console.info(action.type, action);
+    next(action);
+  };
+};
+
+export default logger
--- src/store/store-config.js
+++ src/store/store-config.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware  } from 'redux'
 import thunk from 'redux-thunk'
+import logger from '../midleware/logger'
 import rootReducer from '../reducers'
 
 export default function StoreConfig(preloadedState) {
-  const finalCreateStore = applyMiddleware(thunk)(createStore);
+  const finalCreateStore = applyMiddleware(thunk, logger )(createStore);
   const store = finalCreateStore(rootReducer, preloadedState);
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
